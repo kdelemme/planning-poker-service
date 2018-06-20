2c0111a96142646a4a3083e322fa51381f76c147
Kevin Delemme <kevin.delemme@transferwise.com>
2018-06-20 12:10:09 -0400

    Better image versioning

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -3,4 +3,4 @@
 docker build --rm -t kdelemme/planning-poker-service .
 
 docker tag kdelemme/planning-poker-service \
-  168458823459.dkr.ecr.eu-west-1.amazonaws.com/kdelemme/planning-poker-service
+  168458823459.dkr.ecr.eu-west-1.amazonaws.com/kdelemme/planning-poker-service:latest
